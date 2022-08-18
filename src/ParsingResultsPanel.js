d29865a13358eb417f138e90c45dd150d09206d6
Mark Skroba <mark.skroba@slu.edu>
2022-08-18 11:58:02 -0500

    basic loading for table

--- src/ParsingResultsPanel.js
+++ src/ParsingResultsPanel.js
@@ -7,29 +7,41 @@ import ResultsTable from "./ResultsTable";
 const ParsingResultsPanel = () => {
     const [active, setActive] = useState("Xbox Game Console");
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const updateActive = (newActive) => {
         setActive(newActive);
     };
 
     useEffect(() => {
+        setLoading(true)
         console.log(active);
 
         axios
             .get(`https://gameparser-server.herokuapp.com/update/${active}`)
             .then((res) => {
+                setLoading(false)
                 setData(res.data);
             })
             .catch((error) => {
+                setLoading(false)
                 setData([]);
             });
     }, [active]);
+
+    let table;
+    if (loading) {
+        table = <h1>Loading</h1>
+    } else {
+        table = <ResultsTable data={data} />
+    }
+
 
     return (
         <section>
             <ResultsButtons updateActive={updateActive} active={active} />
             <div id="filter"></div>
-            <ResultsTable data={data} />
+            {table}
         </section>
     );
 };
